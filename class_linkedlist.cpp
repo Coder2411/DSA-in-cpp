690aee7c205509b1d9d2b239bbb0d13d11fcd1a6
Coder2411 <98026546+Coder2411@users.noreply.github.com>
2026-08-12 10:47:20 +0530

    Add files via upload

---
+++ class_linkedlist.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+//Node class to represent a node of the linked list
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    //Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    //Parametrised constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+//Linked list class to implement a singly linked list
+class Linkedlist {
+    Node* head;
+
+    public:
+    //Default constructor
+    Linkedlist() {
+        head = NULL;
+    }
+
+    //Function to insert a node at the start of the linked list
+    void insertAtHead(int data) {
+
+        //Create the new node
+        Node* newNode = new Node(data);
+
+        //Assign to head if list is empty
+        if (head == NULL) {
+            head = newNode;
+            return;
+        }
+
+        //Insert the newly created linked list at the head
+        newNode->next = this->head;
+        this->head = newNode;
+    }
+
+    //Function to print the linked list
+    void print() {
+        Node* temp = head;
+
+        //Check for empty list
+        if (head == NULL) {
+            cout << "List empty" << endl;
+            return;
+        }
+
+        //Traverse the linked list
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+};
+
+int main() {
+
+    //Creating a Linkedlist object
+    Linkedlist list;
+    
+    //Inserting nodes
+    list.insertAtHead(4);
+    list.insertAtHead(3);
+    list.insertAtHead(2);
+    list.insertAtHead(1);
+
+    cout << "Elements of the list are: ";
+
+    //Print the list
+    list.print();
+    cout << endl;
+
+    return 0;
+}
+
+//CREDITS: GeeksForGeeks
